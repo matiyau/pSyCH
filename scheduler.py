bc81c06a890188ef91c0d3762e548d6995ad6ec2
Nishad Mandlik <mandliksg@gmail.com>
2021-04-17 18:26:32 +0200

    Improvements

--- scheduler.py
+++ scheduler.py
@@ -9,7 +9,7 @@ Created on Sat Apr 17 08:08:44 2021
 import bisect as bs
 
 
-class Sched():
+class Generic():
     def __init__(self):
         self.tasks = []
         self.crit_tms = [0]
@@ -18,7 +18,13 @@ class Sched():
     def register_task(self, task):
         self.tasks.append(task)
 
+    def upd_prio_order(self, current_time):
+        self.prio_queue = [task for task in self.tasks]
+
     def create(self, end_time):
+        for task in self.tasks:
+            task.reset()
+        self.crit_tms = [0]
         while True:
             current_time = self.crit_tms[0]
             if (current_time >= end_time):
@@ -26,9 +32,10 @@ class Sched():
             self.crit_tms = self.crit_tms[1:]
             if (len(self.crit_tms) == 0):
                 self.crit_tms = [current_time]
-            self.prio_queue = self.get_prio_order(current_time, self.tasks)
+            self.upd_prio_order(current_time)
             for task in self.prio_queue:
-                used_time, crit_tm = task.sanction(self.crit_tms[0] -
+                used_time, crit_tm = task.sanction(current_time,
+                                                   self.crit_tms[0] -
                                                    current_time)
                 if crit_tm not in self.crit_tms:
                     bs.insort(self.crit_tms, crit_tm)
--- task.py
+++ task.py
@@ -11,12 +11,18 @@ import numpy as np
 
 class Generic():
     def __init__(self, name, tm_params):
-        self.unit = np.gcd.reduce([pair[1] for pair in tm_params.items])
+        self.unit = np.gcd.reduce([pair[1] for pair in tm_params.items()])
         for key in tm_params:
             setattr(self, key, tm_params[key])
         self.exec_logs = np.array([[], []])
 
+    def reset(self):
+        self.exec_logs = np.array([[], []])
+
     def query(self, current_time):
+        return current_time + self.unit
+
+    def get_absolute_deadline(self, current_time):
         return current_time + self.unit
 
     def run(self, current_time, exec_time):
@@ -34,38 +40,45 @@ class Generic():
                                                  [[current_time, current_time,
                                                    current_time + used_time,
                                                    current_time + used_time],
-                                                  [1, 0, 0, 1]]], axis=1)
+                                                  [0, 1, 1, 0]]], axis=1)
         return used_time, self.query(current_time+used_time)
-
-    def get_absolute_deadline(self, current_time):
-        return current_time + self.unit
 
 
 class Periodic(Generic):
     def __init__(self, index, c, t, d=None):
         Generic.__init__(self, "Task " + str(index),
                          {"c": c, "t": t, "d": t if d is None else d})
-        self.prev_run_rime = -1
+        self.prev_run_time = -1
+        self.pending_time = 0
+
+    def reset(self):
+        Generic.reset(self)
+        self.prev_run_time = -1
         self.pending_time = 0
 
     def query(self, current_time):
         return (current_time//self.t + 1) * self.t
 
+    def get_absolute_deadline(self, current_time):
+        return current_time//self.t + self.d
+
     def run(self, current_time, available_time):
         self.pending_time += (current_time//self.t -
-                              self.prev_run_time//self.t) * self.t
+                              self.prev_run_time//self.t) * self.c
         self.prev_run_time = current_time
         used_time = min(self.pending_time, available_time)
         self.pending_time -= used_time
         return used_time
 
-    def get_absolute_deadline(self, current_time):
-        return current_time//self.t + self.d
-
 
 class Aperiodic(Generic):
     def __init__(self, index, c, a=0, d=0):
-        Generic.__init__(self, "Task " + str(index), {"a": a, "c": c, "d": d})
+        Generic.__init__(self, "Task " + str(index), {"a": a, "c": c, "d": d,
+                                                      "c_rem": c})
+
+    def reset(self):
+        Generic.reset(self)
+        self.c_rem = self.c
 
     def query(self, current_time):
         if (current_time < self.a):
@@ -73,19 +86,19 @@ class Aperiodic(Generic):
         else:
             return -1
 
-    def run(self, current_time, available_time):
-        if (current_time >= self.a):
-            used_time = min(self.c, available_time)
-            self.c -= used_time
-            return used_time
-        else:
-            return 0
-
     def get_absolute_deadline(self, current_time):
         if (current_time >= self.a):
             return self.a + self.d
         else:
             return -1
+
+    def run(self, current_time, available_time):
+        if (current_time >= self.a):
+            used_time = min(self.c_rem, available_time)
+            self.c_rem -= used_time
+            return used_time
+        else:
+            return 0
 
 
 class Server(Generic):
@@ -97,25 +110,29 @@ class Server(Generic):
         self.q_logs = np.array([[], []])
         self.jobs = []
 
-    def modify_job(self):
-        return
-
-    def attach_job(self, job):
-        self.modify(job)
-        self.jobs.append(job)
+    def reset(self):
+        Generic.reset(self)
+        self.q_rem = 0
+        self.q_logs = np.array([[], []])
+        for job in self.jobs:
+            job.reset()
 
     def query(self, current_time):
-        # TODO: Change might be reqd
         crit_tm_self = self.get_self_crit_tm(current_time)
         crit_tms_jobs = np.array([job.query(current_time)
                                   for job in self.jobs])
-        return min(crit_tm_self, crit_tms_jobs[crit_tms_jobs >= 0].min())
+        crit_tms = np.concatenate([crit_tm_self, crit_tms_jobs])
+        return crit_tms[crit_tms >= 0].min()
+
+    def get_absolute_deadline(self, current_time):
+        self.modify_all_jobs()
+        ds = np.array([job.get_absolute_deadline for job in self.jobs])
+        ds = ds[ds > current_time]
+        return ds.min() if ds.size != 0 else -1
 
     def run(self, current_time, available_time):
         self.q_logs = np.concatenate([self.q_logs,
                                       [[current_time], [self.q_rem]]], axis=1)
-        # TODO: Write Function and Add additional parameters for updating
-        # budget
         self.update_budget(current_time)
         self.q_logs = np.concatenate([self.q_logs,
                                       [[current_time], [self.q_rem]]], axis=1)
@@ -128,3 +145,24 @@ class Server(Generic):
         self.q_logs = np.concatenate([self.q_logs, [[current_time + used_time],
                                                     [self.q_rem]]], axis=1)
         return used_time
+
+    def attach_job(self, job):
+        i = 0
+        while (i < len(self.jobs)):
+            if (self.jobs[i].a > job.a):
+                break
+            i += 1
+        self.jobs.insert(i, job)
+
+    def modify_job(self, job_index):
+        return
+
+    def modify_all_jobs(self):
+        for i in range(0, len(self.jobs)):
+            self.modify_job(i)
+
+    def get_self_crit_tm(self, current_time):
+        return current_time + self.unit
+
+    def update_budget(self, current_time):
+        self.q_rem = self.q
